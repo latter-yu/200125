54d4d674343d67201f5cb112ca78fed2af25864d
plum blossom <1447394437@qq.com>
2020-01-25 13:35:32 +0800

    提交代码

---
+++ 200125/200125/test.c
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+struct stu
+{
+	int num;
+	char name[10];
+	int age;
+};
+void fun(struct stu *p)
+{
+	printf("%s\n", (*p).name);
+	return;
+}
+int main()
+{
+	struct stu students[3] = { { 9801, "zhang", 20 },
+	{ 9802, "wang", 19 },
+	{ 9803, "zhao", 18 }
+	};
+	fun(students + 1);
+	return 0;
+}
+
+#include <stdio.h>
+int main()
+{
+	printf("hello kid\n");
+	return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(long double));
+	return 0;
+
+
+#include <stdio.h>
+	int main()
+	{
+		int num1 = 10;
+		int num2 = 20;
+		scanf("%d %d", &num1 &num2);
+		printf(%d\n)
+			int sum = 0;
+		sum = num1 + num2;
+		printf("sum=%d\n", sum);
+		return 0;
+	}
+
+
+#include <stdio.h>
+	int sum(int a)
+	{
+		int c = 0;
+		static int b = 3;
+		c += 1;
+		b += 2;
+		return (a + b + c);
+	}
+	int main()
+
+	{
+		int i;
+		int a = 2;
+		for (i = 0; i < 5; i++)
+		{
+			printf("%d, ", sum(a));
+		}
+	}
+	int get_max(int x, int y)
+	{
+		int z;
+		z = x > y ? x : y;
+		return z;
+
+	}
+	int main()
+	{
+		int num1 = 10;
+		int num2 = 20;
+		//要求：完成get_max函数
+		int max = get_max(num1, num2);
+		int get_max(int x, int y)
+
+			int get_max(int x, int y)
+		{
+				int z;
+				z = x > y ? x : y;
+				return z;
+
+			}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+		int main()
+		{
+
+			int num1 = 0;
+			int num2 = 0;
+			int sum = 0;
+			printf("输入两个数字:>");
+			scanf("%d %d", &num1, &num2);
+			sum = num1 + num2;
+			printf("sum=%d\n", sum);
+			return 0;
+		}
